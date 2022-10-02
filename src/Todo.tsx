0693702b1bab57b56fc724a923f1276f778b189e
Federico <muneramulas@hotmail.com>
2022-10-02 19:57:27 +0200

    update style

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -14,57 +14,84 @@ export function Todo(): JSX.Element {
 
   return (
     <div className="container">
-      <div className="instructions">
+      <div className="content">
+        <div className="inputs">
+          <div className="box"></div>
+          <input
+            placeholder="Enter your task..."
+            value={todo}
+            onChange={handleChange}
+          ></input>
+          <button onClick={handleSubmit}>Push</button>
+        </div>
+
+        <div className="todo">
+          {list.length > 0 && (
+            <div className="active">
+              <ul>
+                {list.map((elem: string, i: number) => {
+                  return (
+                    <li key={i}>
+                      <div className="box">
+                        <button id="remove" onClick={() => handleRemove(i)}>
+                          -
+                        </button>
+                      </div>
+                      <p>{elem}</p>
+                      <div className="box">
+                        <button id="done" onClick={() => handleCompleted(i)}>
+                          ✓
+                        </button>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+
+          {/* {completed.length > 0 && (
+            <div className="completed">
+              <ul>
+                {completed.map((complete: string[], i: number) => {
+                  return (
+                    <li key={i}>
+                      <button onClick={() => handleRemoveCompleted(i)}>
+                        -
+                      </button>
+                      {complete}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )} */}
+        </div>
+
+        <div className="selection">
+          <div className="numberleft">
+            <p>{list.length} Items Left</p>
+          </div>
+          <div className="select">
+            <p>All</p>
+            <p>Active</p>
+            <p>Completed</p>
+          </div>
+          <div className="reset">
+            <p>Clear Completed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*----------------------------------------------------------------
+<div className="instructions">
         <p>
           Instructions: Insert a task to see the 'Active Tasks' list - Complete
           a task by pressing 'Done' to see the 'Completed Tasks' list. You can
           remove tasks by pressing '-' button
         </p>
       </div>
-
-      <div className="inputs">
-        <input
-          placeholder="Enter your task"
-          value={todo}
-          onChange={handleChange}
-        ></input>
-        <button onClick={handleSubmit}>Push</button>
-      </div>
-
-      <div className="todo">
-        {list.length > 0 && (
-          <div className="active">
-            <h2>Active Tasks: </h2>
-            <ul>
-              {list.map((elem: string, i: number) => {
-                return (
-                  <li key={i}>
-                    <button onClick={() => handleRemove(i)}>-</button>
-                    <button onClick={() => handleCompleted(i)}>Done</button>
-                    {elem}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
-
-        {completed.length > 0 && (
-          <div className="completed">
-            <h2>Completed Tasks: </h2>
-            <ul>
-              {completed.map((complete: string[], i: number) => {
-                return (
-                  <li key={i}>
-                    <button onClick={() => handleRemoveCompleted(i)}>-</button>
-                    {complete}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
+*/
